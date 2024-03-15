58c0d56acba0ff530394481cdba755e014d83a6a
Natasha Rodic <natashawolt@gmail.com>
2024-03-15 18:12:14 -0400

    cached elements

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 /*----- constants -----*/
 const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
+const RPS_LOOKUP = {
+    r: 'https://i.postimg.cc/25mHxSWW/lapis.png',
+    p: 'https://i.postimg.cc/cJmFkG7b/papyrus.png',
+    s: 'https://i.postimg.cc/WzDS4DBB/scalpellus.png',
 
+}
 /*----- app's state (variables) -----*/
 let scores; // Object
 let results;
@@ -8,7 +13,8 @@ let winner;
 
 
 /*----- cached element references -----*/
-
+const pResultEl = document.getElementById('p-result');
+const cResultEl = document.getElementById('c-result')
 
 /*----- event listeners -----*/
 
@@ -30,6 +36,20 @@ function init() {
     render();
 }
 
-function render() {
+function renderScores() {
+    for (let key in scores) {
+        const scoreEl = document.getElementById(`${key}-score`)
+        scoreEl.innerHTML = scores[key];
+    }
 
+
+}
+
+function renderResults() {
+
+}
+
+function render() {
+    renderScores()
+    renderResults()
 }
